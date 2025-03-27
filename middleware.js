e6d72ae4bbadf6d71196f9c1d5276250715d997b
Gilbert Nabaasa ☑️ <159273162+NabaasaGilbert@users.noreply.github.com>
2025-03-27 14:21:46 +0300

    Create middleware.js

---
+++ middleware.js
@@ -0,0 +1,11 @@
+import { NextResponse } from 'next/server';
+
+export async function middleware(req) {
+  const url = req.nextUrl.clone();
+
+  if (url.pathname.startsWith('/start')) {
+    return NextResponse.rewrite('https://162.241.24.152/~simbapr1/uketalearning/start' + url.pathname.replace('/start', ''));
+  }
+
+  return NextResponse.next();
+}
